374f2da4dff05ec9159ae7e2efd9f54e3efe4ebd
Bhea-gibaga <pedroduray@gmail.com>
2025-05-25 10:35:02 +0800

    commit

--- components/loader.tsx
+++ components/loader.tsx
@@ -2,7 +2,7 @@ const Loader = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-75 z-50">
       <div className="relative">
-        <div className="w-12 h-12 border-4 border-primary-600 border-t-transparent border-r-transparent rounded-lg animate-spin"></div>
+        <div className="w-0 h-0 border-l-[20px] border-l-transparent border-r-[20px] border-r-transparent border-b-[35px] border-primary-600 animate-spin"></div>
         <span className="sr-only">Loading...</span>
       </div>
     </div>
